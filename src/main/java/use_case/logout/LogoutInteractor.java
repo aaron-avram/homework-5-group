59e08144d33d4869a6d61ff90dd77b57bcea7f18
dmrgn <danielrmorgan11@gmail.com>
2025-07-10 10:09:03 -0400

    fix null username

--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -18,7 +18,7 @@ public class LogoutInteractor implements LogoutInputBoundary {
         // * get the username out of the input data,
         String username = logoutInputData.getUsername();
         // * set the username to null in the DAO
-        this.userDataAccessObject.setCurrentUsername(username);
+        this.userDataAccessObject.setCurrentUsername(null);
         // * instantiate the `LogoutOutputData`, which needs to contain the username.
         LogoutOutputData logoutOutputData = new LogoutOutputData(username, false);
         // * tell the presenter to prepare a success view.
