8c0c72a3ddc2388207087ff5c20ad646c04d9e78
intelligent-username <108492855+intelligent-username@users.noreply.github.com>
2025-07-11 09:13:03 -0400

    REDOING pull request (same as before) properly for HW

--- src/main/java/interface_adapter/logout/LogoutPresenter.java
+++ src/main/java/interface_adapter/logout/LogoutPresenter.java
@@ -1,8 +1,6 @@
 package interface_adapter.logout;
 
 import interface_adapter.ViewManagerModel;
-import interface_adapter.change_password.LoggedInState;
-import interface_adapter.login.LoginState;
 import interface_adapter.change_password.LoggedInViewModel;
 import interface_adapter.login.LoginState;
 import interface_adapter.login.LoginViewModel;
@@ -21,9 +19,7 @@ public class LogoutPresenter implements LogoutOutputBoundary {
     public LogoutPresenter(ViewManagerModel viewManagerModel,
                            LoggedInViewModel loggedInViewModel,
                            LoginViewModel loginViewModel) {
-        this.loggedInViewModel = loggedInViewModel;
-        this.viewManagerModel = viewManagerModel;
-        this.loginViewModel = loginViewModel;
+        // TODO: assign to the three instance variables.
     }
 
     @Override
@@ -35,21 +31,16 @@ public class LogoutPresenter implements LogoutOutputBoundary {
         // We also need to set the username in the LoggedInState to
         // the empty string.
 
+        // TODO: have prepareSuccessView update the LoggedInState
         // 1. get the LoggedInState out of the appropriate View Model,
-        LoggedInState loggedInState = this.loggedInViewModel.getState();
         // 2. set the username in the state to the empty string
-        loggedInState.setUsername("");
         // 3. set the state in the LoggedInViewModel to the updated state
-        loggedInViewModel.setState(loggedInState);
         // 4. firePropertyChanged so that the View that is listening is updated.
         loggedInViewModel.firePropertyChanged();
 
         // 5. get the LoginState out of the appropriate View Model,
-        LoginState loginState = this.loginViewModel.getState();
         // 6. set the username and password in the state to the empty string
-        loginState.setUsername("");
         // 7. set the state in the LoginViewModel to the updated state
-        loginViewModel.setState(loginState);
         // 8. firePropertyChanged so that the View that is listening is updated.
         loginViewModel.firePropertyChanged();
 
